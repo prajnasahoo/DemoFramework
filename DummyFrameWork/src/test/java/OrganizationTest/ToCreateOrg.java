743d26776603e2aa9852ba601b1d18a650d24e2a
sahoo <sahoo@bubly>
2025-03-28 09:42:04 +0530

    First push

---
+++ DummyFrameWork/src/test/java/OrganizationTest/ToCreateOrg.java
@@ -0,0 +1,10 @@
+package OrganizationTest;
+
+public class ToCreateOrg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DummyFrameWork/src/test/java/contactTest/ToCreateContact.java
@@ -0,0 +1,8 @@
+package contactTest;
+
+public class ToCreateContact {
+	public static void main(String[]args) {
+		System.out.println("Contact Create succufully by Prajna");
+	}
+
+}
